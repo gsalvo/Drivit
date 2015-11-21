562061b9914cda047c4ebc6f0d89f4a2f7a9e0ae
Gustavo Salvo <gsalvo@vasal.cl>
2015-11-20 23:10:06 -0300

    Avance implementación de vistas

--- app/src/main/java/cl/blackbirdhq/drivit/presentationClassC.java
+++ app/src/main/java/cl/blackbirdhq/drivit/presentationClassC.java
@@ -13,25 +13,4 @@ public class presentationClassC extends AppCompatActivity {
         setContentView(R.layout.activity_presentation_class_c);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_presentation_class_c, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
